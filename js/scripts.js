acb26891e490727f39ff9cb7e33d790f905cbf54
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-04-04 09:57:08 -0700

    add form to html page for user to enter place info
    Co-authored-by: Meron G.Tekie <mtekiesd210309@gmail.com>
    Co-authored-by: William Rodriguez <william@rodriguez1.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,6 @@ function Place(location, activity, date, notes, landmarks) {
   this.location = location;
   this.activity = activity;
   this.date = date;
-  this.notes = notes;
   this.landmarks = landmarks;
+  this.notes = notes;
 }
