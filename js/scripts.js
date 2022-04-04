af377cbeb11088be96c01a1bb59876a233a44cf2
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-04-04 09:47:14 -0700

    add Places object script to collect vacation info
    Co-authored-by: Meron G.Tekie <mtekiesd210309@gmail.com>
    Co-authored-by: William Rodriguez <william@rodriguez1.com>

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+function Place(location, activity, date, notes, landmarks) {
+  this.location = location;
+  this.activity = activity;
+  this.date = date;
+  this.notes = notes;
+  this.landmarks = landmarks;
+}
